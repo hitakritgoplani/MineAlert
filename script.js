fbf1e785afe2041a6c5070c9c2ed068c4ca652a3
Krish Mehta <98233063+krishmehta511@users.noreply.github.com>
2023-03-11 14:17:02 +0530

    Navigation and Header/Nav

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-let menuicn = document.querySelector(".menuicn");
-let nav = document.querySelector(".navcontainer");
+$(document).ready(function() {
 
-menuicn.addEventListener("click",()=>
-{
-	nav.classList.toggle("navclose");
-})
+	loadContent('./pages/dashboard.html');
+
+	$('#option0').click(function(event) {
+		event.preventDefault();
+		loadContent('../pages/dashboard.html');
+	  });
+
+	$('#option1').click(function(event) {
+	  event.preventDefault();
+	  loadContent('./pages/dashboard.html');
+	});
+	
+	$('#option2').click(function(event) {
+	  event.preventDefault();
+	  loadContent('./pages/body_temp.html');
+	});
+	
+	$('#option3').click(function(event) {
+	  event.preventDefault();
+	  loadContent('./pages/pulse_rate.html');
+	});
+	
+	$('#option4').click(function(event) {
+		event.preventDefault();
+		loadContent('./pages/fall_sensor.html');
+	});
+
+  });
+  
+  function loadContent(url) {
+	$.ajax({
+	  url: url,
+	  type: 'GET',
+	  success: function(data) {
+		$('#content-container').html(data);
+	  },
+	  error: function() {
+		console.log('Error loading content');
+	  }
+	});
+  }
+  
