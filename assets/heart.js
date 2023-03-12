25467c9c179778ac1176f2e4f7cf0605f459ff17
Hitakrit Goplani <hitakritgoplani@gmail.com>
2023-03-12 22:14:40 +0530

    Fixes

--- assets/heart.js
+++ assets/heart.js
@@ -1,37 +1,47 @@
 function generateItems() {
-  db.collection("miners").orderBy("mid").onSnapshot((snapshot) => {
-    let items = [];
-    snapshot.docs.forEach((doc) => {
-      let status = "";
-      if (doc.data().heartrate > 120) {
-        status = "Very High";
-      } else {
-        status = "Normal";
-      }
-      items.push({
-        id: doc.id,
-        ...doc.data(),
-        status: status,
-      });
-    });
-    generateHtml(items);
-  });
+	db.collection("miners").orderBy("heartrate").onSnapshot((snapshot) => {
+		let items = [];
+		snapshot.docs.forEach((doc) => {
+			let status = "";
+			if (doc.data().heartrate*-1 > 120) {
+				status = "Very High";
+
+			} else {
+				status = "Normal";
+			}
+			items.push({
+				id: doc.id,
+				...doc.data(),
+				status: status,
+			});
+		});
+		generateHtml(items);
+	});
 }
 
 function generateHtml(items) {
-  let itemsHtml = "";
-  items.forEach((item) => {
-    itemsHtml += `
-                <div class="item1">
-                  <h3 class="t-op-nextlvl">${item.mid}</h3>
-                  <h3 class="t-op-nextlvl">${item.gid}</h3>
-                  <h3 class="t-op-nextlvl">${item.mname}</h3>
-                  <h3 class="t-op-nextlvl">${item.heartrate}</h3>
-                  <h3 class="t-op-nextlvl label-tag">${item.status}</h3>
-                </div>
-            `;
-  });
-  document.querySelector(".heart_items").innerHTML = itemsHtml;
+	$('tbody#childTableTag').remove();
+	$('table#new-table').append('<tbody id="childTableTag"></tbody>');
+	let itemsHtml = "";
+	items.forEach((item) => {
+		var itemsHtml = '<tr>' +
+			'<td>' + item.mid + '</td>' +
+			'<td>' + item.gid + '</td>' +
+			'<td>' + item.mname + '</td>' +
+			'<td>' + item.heartrate * -1 + '</td>' +
+			'<td id="current-status">' + item.status + '</td>' +
+			'</tr>';
+
+		// $('#childTableTag').clear();	
+		$('#childTableTag').append(itemsHtml);
+
+	});
+	document.querySelector(".childTableTag").innerHTML = itemsHtml;
+}
+
+function colorchange() {
+	
 }
 
 generateItems();
+colorchange();
