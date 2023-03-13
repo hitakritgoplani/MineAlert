17e89a58ee8c31af53012d0272597fb1ecad0ecd
Hitakrit Goplani <hitakritgoplani@gmail.com>
2023-03-13 12:29:04 +0530

    Body temp

---
+++ assets/body.js
@@ -0,0 +1,54 @@
+function generateItems() {
+	db.collection("miners").orderBy("heartrate").onSnapshot((snapshot) => {
+		let items = [];
+		snapshot.docs.forEach((doc) => {
+			let status = "";
+			if (doc.data().heartrate < 37.6) {
+				status = "Normal";
+
+			} else if(doc.data().heartrate > 37.6 && doc.data().heartrate <= 40 ){
+				status = "High"
+			}
+			else {
+				status = "Very High";
+			}
+			items.push({
+				id: doc.id,
+				...doc.data(),
+				status: status,
+			});
+		});
+		generateHtml(items);
+	});
+}
+
+function generateHtml(items) {
+	$('tbody#childTableTag').remove();
+	$('table#new-table').append('<tbody id="childTableTag"></tbody>');
+	let itemsHtml = "";
+	items.forEach((item) => {
+		if(item.heartrate*-1 > 120){
+			var itemsHtml = '<tr>' +
+			'<td>' + item.mid + '</td>' +
+			'<td>' + item.gid + '</td>' +
+			'<td>' + item.mname + '</td>' +
+			'<td>' + item.heartrate * -1 + '</td>' +
+			'<td style="color:red;">' + item.status + '</td>' +
+			'</tr>';
+		}
+		else{
+			var itemsHtml = '<tr>' +
+			'<td>' + item.mid + '</td>' +
+			'<td>' + item.gid + '</td>' +
+			'<td>' + item.mname + '</td>' +
+			'<td>' + item.heartrate * -1 + '</td>' +
+			'<td style="color:green;">' + item.status + '</td>' +
+			'</tr>';
+		}
+		$('#childTableTag').append(itemsHtml);
+
+	});
+	document.querySelector(".childTableTag").innerHTML = itemsHtml;
+}
+
+generateItems();
