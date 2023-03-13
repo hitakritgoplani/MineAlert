76ec27d8d44b099b6f523f54b8c91dba0e13c4c1
Hitakrit Goplani <hitakritgoplani@gmail.com>
2023-03-13 13:18:15 +0530

    body-temp

--- assets/body.js
+++ assets/body.js
@@ -1,20 +1,23 @@
 function generateItems() {
-	db.collection("miners").orderBy("heartrate").onSnapshot((snapshot) => {
+	let ref = rtdb.ref("miners");
+	ref.on("value", (snapshot) => {
+		let ss = snapshot.val().sort((a, b) => a.temperature - b.temperature);
 		let items = [];
-		snapshot.docs.forEach((doc) => {
+		ss.forEach((element) => {
 			let status = "";
-			if (doc.data().heartrate < 37.6) {
+			if (element.temperature *-1 < 34) {
+				status = "Very Low";
+			} else if(element.temperature *-1 < 37.6 && element.temperature *-1 > 34) {
 				status = "Normal";
-
-			} else if(doc.data().heartrate > 37.6 && doc.data().heartrate <= 40 ){
-				status = "High"
 			}
-			else {
-				status = "Very High";
+			else if(element.temperature *-1 > 37.6 && element.temperature *-1  < 40) {
+				status = "High";
+			}
+			else{
+				status="Very High"
 			}
 			items.push({
-				id: doc.id,
-				...doc.data(),
+				...element,
 				status: status,
 			});
 		});
@@ -23,30 +26,50 @@ function generateItems() {
 }
 
 function generateHtml(items) {
-	$('tbody#childTableTag').remove();
-	$('table#new-table').append('<tbody id="childTableTag"></tbody>');
+	$("tbody#childTableTag").remove();
+	$("table#new-table").append('<tbody id="childTableTag"></tbody>');
 	let itemsHtml = "";
 	items.forEach((item) => {
-		if(item.heartrate*-1 > 120){
-			var itemsHtml = '<tr>' +
-			'<td>' + item.mid + '</td>' +
-			'<td>' + item.gid + '</td>' +
-			'<td>' + item.mname + '</td>' +
-			'<td>' + item.heartrate * -1 + '</td>' +
-			'<td style="color:red;">' + item.status + '</td>' +
-			'</tr>';
+		if (item.temperature *-1 < 37.6) {
+			var itemsHtml =
+				"<tr>" +
+				"<td>" +
+				item.mid +
+				"</td>" +
+				"<td>" +
+				item.gid +
+				"</td>" +
+				"<td>" +
+				item.mname +
+				"</td>" +
+				"<td>" +
+				item.temperature *-1+
+				"</td>" +
+				'<td style="color:green;">' +
+				item.status +
+				"</td>" +
+				"</tr>";
+		} else {
+			var itemsHtml =
+				"<tr>" +
+				"<td>" +
+				item.mid +
+				"</td>" +
+				"<td>" +
+				item.gid +
+				"</td>" +
+				"<td>" +
+				item.mname +
+				"</td>" +
+				"<td>" +
+				item.temperature *-1+
+				"</td>" +
+				'<td style="color:red;">' +
+				item.status +
+				"</td>" +
+				"</tr>";
 		}
-		else{
-			var itemsHtml = '<tr>' +
-			'<td>' + item.mid + '</td>' +
-			'<td>' + item.gid + '</td>' +
-			'<td>' + item.mname + '</td>' +
-			'<td>' + item.heartrate * -1 + '</td>' +
-			'<td style="color:green;">' + item.status + '</td>' +
-			'</tr>';
-		}
-		$('#childTableTag').append(itemsHtml);
-
+		$("#childTableTag").append(itemsHtml);
 	});
 	document.querySelector(".childTableTag").innerHTML = itemsHtml;
 }
